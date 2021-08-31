39b735db43912c7430f56611f7a8892a1d03295f
Amit Alon <amit@kaholo.io>
2021-08-31 15:35:47 +0300

    Added some new methods and created option to store the ssh key used to clone

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,12 @@
+const os = require('os');
 const child_process = require("child_process");
-const compareVersion = require('./compare-versions');
 const GitKey = require('./git-key');
-const homeDirectory = require('os').homedir();
+const fs = require("fs");
+const compareVersion = require('./compare-versions');
+const homeDirectory = os.homedir();
+const isWin = os.platform()=='win32';
+
+const normalize = require('path').normalize;
 
 async function verifyGitVersion(){
     const minimalGitRequired = '2.10.0';
@@ -19,7 +24,7 @@ async function verifyGitVersion(){
       throw `Git version must be ${minimalGitRequired} or higher, got ${gitVersion}`;
     }
 }
-  
+
 async function execCommand(command, opts = {}){
     if (!opts.env) opts.env = process.env;
     return new Promise((resolve,reject) => {
@@ -36,24 +41,75 @@ async function execCommand(command, opts = {}){
     })
 }
 
-async function getSSHCommand(privateKey, isWin){
-    const gitKey = await GitKey.from(privateKey);
-    const keyPathParam = isWin ? gitKey.keyPath.replace(/\\/g,'\\\\') : gitKey.keyPath;
-    return [gitKey, `ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${keyPathParam}`];
+async function execGitCommand(args, path){
+  const opts = path ? {cwd: path} : {};
+  return execCommand(`git ${args.join(" ")}`, opts)
 }
 
-function splitByNewLine(text){
-    return text.split("\n").map(line => line.trim()).filter(line => line);
+async function getSSHCommand(privateKey, saveCreds){
+    const gitKey = await GitKey.from(privateKey, saveCreds);
+    const keyPathParam = isWin ? gitKey.keyPath.replace(/\\/g,'\\\\') : gitKey.keyPath;
+    return [gitKey, `ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${keyPathParam}`];
 }
 
 function untildify(path){
     return homeDirectory ? path.replace(/^~(?=$|\/|\\)/, homeDirectory) : path;
 }
 
+async function tryDelete(path){
+  path = normalize(untildify(path));
+  if (!fs.existsSync(path)) return false;
+  try{
+    const gitKey = await GitKey.fromRepoFolder(path);
+    if (gitKey) await gitKey.dispose();
+  }
+  catch (err){
+    throw `couldn't delete the key`;
+  }
+  finally{
+    try {
+      fs.rmdirSync(path, { recursive: true });
+    } catch (err) { throw `Couldn't delete the repository: ${path}`}
+  }
+  return true;
+}  
+
+async function turnSshAgentUp(gitKey){
+  if (isWin || !gitKey || !gitKey.keyPath) return;
+  // check if ssh agent is down or up
+  let didTurnAgentUp = false;
+  if (!process.env["SSH_AUTH_SOCK"]) {
+      // ssh agent is down so add command to turn it up and save returned env variables
+      const sshAgentResult = await execCommand(`ssh-agent`);
+      const matchResult = sshAgentResult.matchAll(/(SSH_AUTH_SOCK|SSH_AGENT_PID)=([^;]+);/g);
+      process.env["SSH_AUTH_SOCK"] = matchResult.next().value[2];
+      process.env["SSH_AGENT_PID"] = matchResult.next().value[2];
+      didTurnAgentUp = true;
+  }
+  // add key to ssh agent
+  await execCommand(`ssh-add ${gitKey.keyPath}`);
+  return didTurnAgentUp;
+}
+
+async function killSshAgent(){
+  try { await execCommand(`eval \`ssh-agent -k\``); }
+  catch (err) {}
+}
+
+async function setUsernameAndEmail(username, email, path){
+  if (username) await execGitCommand([`config user.name "${username}"`], path);
+  if (email) await execGitCommand([`config user.email "${email}"`], path);
+}
+
 module.exports = {
-    verifyGitVersion,
-    execCommand,
-    splitByNewLine,
-    getSSHCommand,
-    untildify
+  verifyGitVersion,
+  execCommand,
+  getSSHCommand,
+  untildify,
+  tryDelete,
+  turnSshAgentUp,
+  killSshAgent,
+  execGitCommand,
+  setUsernameAndEmail,
+  isWin
 };
